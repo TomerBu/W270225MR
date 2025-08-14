a6f733484c5339719a9acfcb1898aa9ac705974d
TomerBu <TomerBu@gmail.com>
2025-08-14 13:58:33 +0300

    comp v2

--- Python/Lec2/list_comp.py
+++ Python/Lec2/list_comp.py
@@ -1,20 +1,12 @@
-# list comprehension
-
-"""
-list of squares:
-[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
-"""
-
-squares = []
+# numbers = [i for i in range(10)]
+numbers = []
 
 for i in range(10):
-    squares += [i**2]
+    numbers.append(i)
 
-print(squares)
-
-print([n**2 for n in range(10)])
+print(numbers)
 
 
-numbers = [i for i in range(10)]
+print([i for i in range(10)])
 
-print(numbers) #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print([i**2 for i in range(10)])
