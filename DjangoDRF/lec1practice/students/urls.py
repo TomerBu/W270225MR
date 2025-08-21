46de1e22f2cbabf4747bc89edb65f22ce9981ecd
TomerBu <TomerBu@gmail.com>
2025-08-21 15:08:49 +0300

    lec1

--- DjangoDRF/lec1practice/students/urls.py
+++ DjangoDRF/lec1practice/students/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from students.views import all, me
+from students.views import all, me, get_names, add_name
 
 urlpatterns = [
     path('all/', all, name='all'),
     path('me/', me, name='me'),
+    path('names/', get_names, name='getnames'),
+    path('add_name/', add_name, name='add_name'),
 ]
--- DjangoDRF/lec1practice/students/views.py
+++ DjangoDRF/lec1practice/students/views.py
@@ -1,7 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.views.decorators.csrf import csrf_exempt
+names = ['Dani', 'Alice', 'Bob']
+
 
 def all(request):
     return HttpResponse("all")
 
+
 def me(request):
     return HttpResponse("me")
+
+
+def get_names(request):
+    # return 'Dani Alice Bob'
+    return HttpResponse(" ".join(names))
+
+
+@csrf_exempt  #temp disable CSRF verification
+def add_name(request:HttpRequest):
+    if request.method == 'POST':
+        return HttpResponse("POST")
+    else:
+        return HttpResponse("Not POST", status=403)
+
+
+# http://127.0.0.1:8000/students/add_name/
