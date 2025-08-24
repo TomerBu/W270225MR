e1021cf0cb8dcc848e5293f8c4ec6d40073ea654
TomerBu <TomerBu@gmail.com>
2025-08-24 13:42:32 +0300

    lec2

--- DjangoDRF/lec2/quotes/models.py
+++ DjangoDRF/lec2/quotes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    
+
+class Quote(models.Model):
+    # props:
+    author = models.CharField(max_length=60)
+    quote = models.TextField()
+    year = models.IntegerField()
+
+    # admin timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- DjangoDRF/lec2/quotes/views.py
+++ DjangoDRF/lec2/quotes/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-quotes = ["Life is what happens when you're busy making other plans.",
-          "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-          "In the end, we will remember not the words of our enemies, but the silence of our friends.˝"]
-
+from quotes.models import Quote
+ 
 
 def get_quotes(req):
-    return HttpResponse("<hr>".join(quotes))
-
-
-def get_single_quote(req, id):
-    return HttpResponse(quotes[id])
+    quotes = Quote.objects.all()
+    return render(req, 'quotes/list.html' , {'quotes': quotes})
