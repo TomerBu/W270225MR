cee5d5224255f82cc25b6927bc7a3031720a73f4
TomerBu <TomerBu@gmail.com>
2025-08-24 10:24:13 +0300

    lec2

--- DjangoDRF/lec2/quotes/urls.py
+++ DjangoDRF/lec2/quotes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from quotes.views import get_quotes
 
 urlpatterns = [
-    path('', get_quotes)
+    path('', get_quotes),
 ]
--- DjangoDRF/lec2/quotes/views.py
+++ DjangoDRF/lec2/quotes/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def get_quotes(request):
-    return HttpResponse('Mike')
+
+quotes = ["Life is what happens when you're busy making other plans.",
+          "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+          "In the end, we will remember not the words of our enemies, but the silence of our friends.˝"]
+
+
+def get_quotes(req):
+    return HttpResponse("<hr>".join(quotes))
+
+
+def get_single_quote(req, id):
+    return HttpResponse(quotes[id])
