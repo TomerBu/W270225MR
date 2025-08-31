6b4cb64c6cb71d8fb173f3b98eb3d1e7c0a05f24
TomerBu <TomerBu@gmail.com>
2025-08-31 10:35:54 +0300

    quotes html

--- DjangoDRF/lec2/quotes/urls.py
+++ DjangoDRF/lec2/quotes/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from quotes.views import add_quote, get_quotes
+from quotes.views import add_quote, get_quotes, delete_quote, edit_quote
 
 urlpatterns = [
+    # /quotes/
     path('', get_quotes, name='quotes'),
-    path('add', add_quote, name='add_quote')
+
+    # /quotes/add
+    path('add', add_quote, name='add_quote'),
+
+    # /quotes/delete/2
+    path('delete/<int:id>', delete_quote, name='delete_quote'),
+
+    # /quotes/edit/3
+    path('edit/<int:id>', edit_quote, name='edit_quote'),
 ]
--- DjangoDRF/lec2/quotes/views.py
+++ DjangoDRF/lec2/quotes/views.py
@@ -4,22 +4,41 @@ from django.http import HttpRequest
 
 
 def get_quotes(req):
+    # get all quotes from db
     quotes = Quote.objects.all().order_by('-year')
     return render(req, 'quotes/list.html', {'quotes': quotes})
 
 
+# TODO: add 404 if quote not found
+def delete_quote(req: HttpRequest, id):
+  quote = Quote.objects.get(id=id)
+  quote.delete()
+  return redirect('/quotes/')
+
+# TODO: add 404 if quote not found
+def edit_quote(req: HttpRequest, id):
+   quote = Quote.objects.get(id=id)
+
+   if req.method == "GET":
+      return render(req, "quotes/edit.html", {"quote": quote})
+
+   quote.author = req.POST.get('author', quote.author)
+   quote.quote = req.POST.get('quote', quote.quote)
+   quote.year = req.POST.get('year', quote.year)
+   quote.save()
+   return redirect('/quotes/')
+
+
 def add_quote(req: HttpRequest):
-    if req.method == 'POST':
-        author = req.POST.get('author')
-        quote = req.POST.get('quote')
-        year = req.POST.get('year')
+    if req.method == "GET":
+      return render(req, "quotes/add.html")
+    
+    # ניקח את המידע מהטופס - וניצור קווט חדש ונשמור!
+    author = req.POST.get('author')
+    quote = req.POST.get('quote')
+    year = req.POST.get('year')
 
-        if author and quote and year:
-            quote = Quote(author=author, quote=quote, year=year)
-            quote.save()
-        else:
-            return render(req, 'quotes/add.html', {'error': 'must fill all the fields'})
+    q = Quote(quote = quote, author = author, year = year)
+    q.save()
 
-        return redirect('quotes')
-
-    return render(req, 'quotes/add.html')
+    return redirect('/quotes/')
