0c8fa47800eb122e1f1aba400bf47e61bdc3145e
TomerBu <TomerBu@gmail.com>
2025-09-02 12:16:59 +0300

    validation

--- DjangoDRF/lec4/api/serializers.py
+++ DjangoDRF/lec4/api/serializers.py
@@ -1,17 +1,21 @@
 from rest_framework import serializers
 from api.models import Student
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
 class StudentSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
-    age = serializers.IntegerField()
+    age = serializers.IntegerField(
+        validators=[MinValueValidator(4), MaxValueValidator(120)])
 
-    # https://www.django-rest-framework.org/api-guide/serializers/
-    # POST
     def create(self, validated_data):
         return Student.objects.create(**validated_data)
 
-    # PUT
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.age = validated_data.get('age', instance.age)
         instance.save()
         return instance
+
+
+# https://www.django-rest-framework.org/api-guide/serializers/
