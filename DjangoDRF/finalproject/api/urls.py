60558477cf12795c0d1abc12fa469dfc46216466
TomerBu <TomerBu@gmail.com>
2025-09-04 12:10:06 +0300

    api views demo

--- DjangoDRF/finalproject/api/urls.py
+++ DjangoDRF/finalproject/api/urls.py
@@ -1,6 +1,8 @@
-from api.views import say_hello
+from api.views import say_hello, Students, StudentsById
 from django.urls import path
 
 urlpatterns = [
     path('hello/', say_hello),
+    path('students/', Students.as_view(), name='students'),
+    path('students/<int:pk>/', StudentsById.as_view(), name='student_detail'),
 ]
--- DjangoDRF/finalproject/api/views.py
+++ DjangoDRF/finalproject/api/views.py
@@ -6,7 +6,30 @@ from rest_framework.request import Request
 from rest_framework.decorators import api_view
 # Create your views here.
 
-
+# function based views
 @api_view(['GET'])
 def say_hello(request: Request) -> Response:
     return Response({"message": "Hello, world!"})
+
+
+from rest_framework.views import APIView
+class Students(APIView):
+    def get(self, request: Request) -> Response:
+        return Response({"message": "GET"})
+
+    def post(self, request: Request) -> Response:
+        return Response({"message": "POST"})
+    
+
+class StudentsById(APIView):
+    def get(self, request: Request, pk: int) -> Response:
+        return Response({"message": "GET"})
+
+    def post(self, request: Request, pk: int) -> Response:
+        return Response({"message": "POST"})
+
+    def delete(self, request: Request, pk: int) -> Response:
+        return Response({"message": "DELETE"})
+
+    def put(self, request: Request, pk: int) -> Response:
+        return Response({"message": "PUT"})
