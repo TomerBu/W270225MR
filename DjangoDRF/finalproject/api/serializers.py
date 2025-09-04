973f9b5148b3b7b3215d54b6b9320a9c9f83f420
TomerBu <TomerBu@gmail.com>
2025-09-04 11:57:57 +0300

    serializers

--- DjangoDRF/finalproject/api/serializers.py
+++ DjangoDRF/finalproject/api/serializers.py
@@ -1,4 +1,5 @@
-from api.models import UserProfile, Tag
+from api.models import Post, UserProfile, Tag, PostUserLikes, Comment
+from django.contrib.auth.models import User
 from rest_framework.serializers import ModelSerializer
 
 # json + create/update + validations 
@@ -13,3 +14,24 @@ class TagSerializer(ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
+
+class PostSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class PostUserLikesSerializer(ModelSerializer):
+    class Meta:
+        model = PostUserLikes
+        fields = '__all__'
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
