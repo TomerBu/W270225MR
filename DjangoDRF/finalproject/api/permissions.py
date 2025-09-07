b48cbf583fd1f46adc0d5289cb20f04bddbb0279
TomerBu <TomerBu@gmail.com>
2025-09-07 12:27:05 +0300

    comments permissions fixed

--- DjangoDRF/finalproject/api/permissions.py
+++ DjangoDRF/finalproject/api/permissions.py
@@ -2,6 +2,14 @@ from rest_framework import permissions
 
 
 class CommentOwnerOrReadOnly(permissions.BasePermission):
+
+    # /api/comments
+    def has_permission(self, request, view):
+         if request.method in permissions.SAFE_METHODS:
+            return True
+         return request.user and request.user.is_authenticated
+
+    # /api/comments/{id}
     def has_object_permission(self, request, view, obj):
 
         if request.method in permissions.SAFE_METHODS:
