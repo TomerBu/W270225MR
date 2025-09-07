a7783150c9c1420e27c08d1bf36da9aaf8b4e8cd
TomerBu <TomerBu@gmail.com>
2025-09-07 13:48:28 +0300

    added jwt with custom claims

--- DjangoDRF/finalproject/finalproject/settings.py
+++ DjangoDRF/finalproject/finalproject/settings.py
@@ -44,10 +44,15 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    
+
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
